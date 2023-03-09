75312ef3bdc9b6b2ec7615702d4b153e2685dd98
luna <dichotomy.bgm@gmail.com>
2023-03-09 15:39:08 +0900

    spring security 마무리

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,28 +1,51 @@
 package com.cos.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.cos.blog.config.auth.PricipalDetailService;
 
 @Configuration
 @EnableWebSecurity // 시큐리티 필터 등록
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근하면 권한과 인증을 미리 체크
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private PricipalDetailService pricipalDetailService;
+	
+	@Bean
+	public BCryptPasswordEncoder encodePWD() {
+		
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(pricipalDetailService).passwordEncoder(encodePWD());
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http
+			.csrf().disable() // csrf 토큰 비활성화
 			.authorizeRequests()
-				.antMatchers("/auth/**")
+				.antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**")
 				.permitAll()
 				.anyRequest()
 				.authenticated()
 			.and()
 				.formLogin()
-				.loginPage("/auth/loginForm");
+				.loginPage("/auth/loginForm")
+				.loginProcessingUrl("/auth/loginProc")
+				.defaultSuccessUrl("/");
 	
 	}
 	
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,9 +1,11 @@
 package com.cos.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
@@ -13,16 +15,18 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
 	@Transactional
-	public int 회원가입(User user) {
+	public void 회원가입(User user) {
 
-		try {
-			userRepository.save(user);
-			return 1;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return -1;
+		String rawPassword = user.getPassword();
+		String encPassword = encoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		user.setRole(RoleType.USER);
+		userRepository.save(user);
+		
 	}
 
 //	@Transactional(readOnly = true)
