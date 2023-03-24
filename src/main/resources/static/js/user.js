7076fc567aa893030efe73976aa96b27beefe5b4
luna <dichotomy.bgm@gmail.com>
2023-03-24 11:26:24 +0900

    회원가입 시 duplicate 문제 해결

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -24,8 +24,13 @@ let index = {
 			contentType : "application/json; charset=utf-8",
 			dataType : "json"
 		}).done(function(resp){
+		
+		if ( resp.status === 500 ){
+			alert("회원가입이 실패하였습니다");
+		} else {
 			alert("회원가입이 완료되었습니다");
 			location.href = "/";
+		}		
 		}).fail(function(error){
 			alert(JSON.stringify(error));
 		});
